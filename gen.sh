ee51a4975bd40733c85866438f691acd33eabf8d
Vladimir Tolstov <93446392+githubVladimirT@users.noreply.github.com>
2026-02-15 17:15:43 +0300

    Commented lines removed. Format openapi chenged to yml

--- gen.sh
+++ gen.sh
@@ -1,10 +1,3 @@
-# #!/bin/sh -e
-
-# INC=$(go list -f '{{ .Dir }}' -m go.unistack.org/micro-proto/v3)
-# ARGS="-I${INC}"
-
-# protoc $ARGS -Iproto --openapiv3_out=disable_default_errors=true,allow_merge=true:./proto/ --go_out=paths=source_relative:./proto/ --micro_out=components="micro|http",debug=true,paths=source_relative:./proto/ proto/*.proto
-
 #!/bin/sh -e
 
 INC=$(go list -f '{{ .Dir }}' -m go.unistack.org/micro-proto/v3)
@@ -14,4 +7,4 @@ protoc $ARGS -Iproto --go_out=paths=source_relative:./proto/ proto/*.proto
 
 protoc $ARGS -Iproto --go-micro_out=components="micro|http",debug=true,tag_path=./proto/,paths=source_relative:./proto/ proto/*.proto
 
-protoc $ARGS -Iproto --go-micro_out=openapi_file=apidocs.swagger.json,components="openapiv3",debug=true,paths=source_relative:./proto/ proto/*.proto
+protoc $ARGS -Iproto --go-micro_out=openapi_file=apidocs.swagger.yml,components="openapiv3",debug=true,paths=source_relative:./proto/ proto/*.proto
